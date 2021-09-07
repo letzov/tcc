3b87efbb4ba9318be8bd6e9223b60841ffa36dc2
letzov <49003197+letzov@users.noreply.github.com>
2021-09-07 17:03:25 -0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -6,78 +6,40 @@ from typing import Any, Text, Dict, List ## Datatypes
 from rasa_sdk import Action, Tracker  ##
 from rasa_sdk.executor import CollectingDispatcher
 
-pfeizer [
-["número de doses",2],
-["eficacia após primeira dose", 61%],
-["eficacia após duas doses",85%],
-["efeitos colaterais","dor no corpo, no local da injeção, dor de cabeça e fadiga"],
-["país de origem","EUA e Alemanha"]
-["aprovada pela anvisa","Sim"],
-["eficácia contra variantes","88%"],
-["como é feita", ],
-["intervalo entre doses","21 dias"],
-["eficácia por faixa etária","acima de 55 anos, 94%, entre 16 e 55 anos 95%"]
-]
-aztrazeneca [
-["número de doses",2],
-["eficacia após primeira dose",76%],
-["eficacia após duas doses",81%],
-["efeitos colaterais","dor no corpo, no local da injeção, dor de cabeça e fadiga"],
-["país de origem","Inglaterra"]
-["aprovada pela anvisa","Sim"],
-["eficácia contra variantes", 67%],
-["como é feita",],
-["intervalo entre doses"","90 dias"],
-["eficácia por faixa etária","acima de 80 anos, 90%, entre 60 e 79 anos, 93%, entre 18 e 59 anos, 60%"]
-]
-coronavac [
-["número de doses",2],
-["eficacia após primeira dose",76%],
-["eficacia após duas doses",63%]
-["efeitos colaterais","dor no corpo, no local da injeção, dor de cabeça e fadiga"],
-["país de origem","Brasil e China"]
-["aprovada pela anvisa","Sim"],
-["eficácia contra variantes", ""],
-["como é feita",],
-["intervalo entre doses",""],
-["eficácia por faixa etária",""]
-]
-janssen
-["número de doses",1,
-["eficacia após primeira dose",85%],
-["eficacia após duas doses","dose única"],
-["efeitos colaterais","dor no corpo, no local da injeção, dor de cabeça,fadiga e calafrios"],
-["país de origem","EUA"]
-["aprovada pela anvisa","Sim"],
-["eficácia contra variantes", 67%],
-["como é feita",""],
-["intervalo entre doses","dose única"],
-["eficácia por faixa etária","acima de 59 anos, 76%, entre 18 e 59 anos,64%"]
-]
-moderna
-["número de doses",2],
-["eficacia após primeira dose",94%],
-["eficacia após duas doses"94%,],
-["efeitos colaterais","dor no corpo, no local da injeção, vermelhidão, náusea e cansaço"],
-["país de origem","EUA"]
-["aprovada pela anvisa","Sim"],
-["eficácia contra variantes", 94%],
-["como é feita",""],
-["intervalo entre doses","28 dias"],
-["eficácia por faixa etária","acima de 65 anos, 86%, entre 18 e 64 anos,95%"]
-]
-sputnik
-["número de doses",2],
-["eficacia após primeira dose",""],
-["eficacia após duas doses"92%,],
-["efeitos colaterais","dor no corpo, no local da injeção, vermelhidão, náusea e cansaço"],
-["país de origem","Rússia"]
-["aprovada pela anvisa","Não"],
-["eficácia contra variantes", ""],
-["como é feita",""],
-["intervalo entre doses","21 dias"],
-["eficácia por faixa etária","De acordo com o laboratório responsável pela sputink, a eficácia da vacina é igual para todas as faixas etárias"]
-]
+class all_of_them(Action): # pode ser qualquer coisa, mas por padrão utilizaremos o nome da ação
+
+    def name(self) -> Text:
+        return "utter_name_all" # precisa ser o mesmo nome da ação utilizada nas stories e domain file
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        vacinas = {
+        'Astrazeneca': {'numero de doses': 2, 'eficiencia': {'1 dose':'76%', '2 dose': '81%'}, 'eficiencia contra variantes': '92%', 'intervalo entre as doses': '3 meses', 'efeitos colaterais': 'Dor no local da injeção, dor de cabeça, fadiga, dor no corpo e mal esta'},
+        'Pfizer': {'numero de doses': 2, 'eficiencia': {'1 dose':'61%', '2 dose': '88%'}, 'eficiencia contra variantes': '88%', 'intervalo entre as doses': '3 meses', 'efeitos colaterais':'Dor no local da injeção, dor de cabeça, fadiga, dor no corpo e mal estar'},
+        'Coronavac': {'numero de doses': 2, 'eficiencia': {'1 dose':'sem dados%', '2 dose': '63%'}, 'eficiencia contra variantes': 'entre 69 e 78%', 'intervalo entre as doses': '21 dias', 'efeitos colaterais':'Dor no local da injeção, febre, cansaço e calafrios'},
+        'Janssen': {'numero de doses': 1, 'eficiencia': {'1 dose':'85%', '2 dose': 'dose única'}, 'eficiencia contra variantes': '85%', 'intervalo entre as doses': 'dose única', 'efeitos colaterais':'Dor no local da injeção, vermelhidão, dores musculares, febre, náusea, dor no peito e falta de ar'},
+        'Moderna': {'numero de doses': 2, 'eficiencia': {'1 dose':'94%', '2 dose': '94%'}, 'eficiencia contra variantes': '94%', 'intervalo entre as doses': '28 dias', 'efeitos colaterais':'Dor no local injeção, febre, vermelhidão, náusea e cansaço'},
+        'Sputnik': {'numero de doses': 2, 'eficiencia': {'1 dose':'sem dados', '2 dose': '92%'}, 'eficiencia contra variantes': '81%', 'intervalo entre as doses': '21 dias', 'efeitos colaterais':'Dor no local injeção, febre, vermelhidão, náusea e cansaço'}
+        }
+        nome1 = 'Astrazeneca'
+        nome2 = 'Pfizer'
+        nome3 = 'Coronavac'
+        nome4 = 'Janssen'
+        nome5 = 'Moderna'
+        nome6 = 'Sputnik'
+        if all == "todas":
+            caracteristica = 'eficiencia contra variantes'
+            dispatcher.utter_message(text = 'a vacina' + str(nome1)+'possui'+ str(caracteristica)+ str(vacinas[nome1][caracterisca])) 
+        #dispatcher.utter_message(text = 'a vacina' + str(nome2)+'possui'+ str(caracteristica)+ str(vacinas[nome2][caracterisca]))
+        #dispatcher.utter_message(text = 'a vacina' + str(nome3)+'possui'+ str(caracteristica)+ str(vacinas[nome3][caracterisca]))
+        #dispatcher.utter_message(text = 'a vacina' + str(nome4)+'possui'+ str(caracteristica)+ str(vacinas[nome4][caracterisca]))
+        #dispatcher.utter_message(text = 'a vacina' + str(nome5)+'possui'+ str(caracteristica)+ str(vacinas[nome5][caracterisca]))
+        #dispatcher.utter_message(text = 'a vacina' + str(nome6)+'possui'+ str(caracteristica)+ str(vacinas[nome6][caracterisca]))
+        else: 
+            all == ""
+            
+        return []
 
 
 
