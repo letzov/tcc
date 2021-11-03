3e9e0017fb9cadec910e5f104967b0cc94062b69
letzov <49003197+letzov@users.noreply.github.com>
2021-11-02 22:26:00 -0300

    Add files via upload

--- actions.py
+++ actions.py
@@ -9,12 +9,12 @@ from rasa_sdk.types import DomainDict
 
 
 vacinas = {
-'Astrazeneca': {'numero de doses': 2, 'eficiencia': {'1 dose':'76%', '2 dose': '81%'}, 'eficiencia contra variantes': '92%', 'intervalo entre as doses': '3 meses', 'efeitos colaterais': 'Dor no local da injeção, dor de cabeça, fadiga, dor no corpo e mal esta'},
-'Pfizer': {'numero de doses': 2, 'eficiencia': {'1 dose':'61%', '2 dose': '88%'}, 'eficiencia contra variantes': '88%', 'intervalo entre as doses': '3 meses', 'efeitos colaterais':'Dor no local da injeção, dor de cabeça, fadiga, dor no corpo e mal estar'},
-'Coronavac': {'numero de doses': 2, 'eficiencia': {'1 dose':'sem dados%', '2 dose': '63%'}, 'eficiencia contra variantes': 'entre 69% e 78%', 'intervalo entre as doses': '21 dias', 'efeitos colaterais':'Dor no local da injeção, febre, cansaço e calafrios'},
-'Janssen': {'numero de doses': 1, 'eficiencia': {'1 dose':'85%', '2 dose': 'dose única'}, 'eficiencia contra variantes': '85%', 'intervalo entre as doses': 'dose única', 'efeitos colaterais':'Dor no local da injeção, vermelhidão, dores musculares, febre, náusea, dor no peito e falta de ar'},
-'Moderna': {'numero de doses': 2, 'eficiencia': {'1 dose':'94%', '2 dose': '94%'}, 'eficiencia contra variantes': '94%', 'intervalo entre as doses': '28 dias', 'efeitos colaterais':'Dor no local injeção, febre, vermelhidão, náusea e cansaço'},
-'Sputnik': {'numero de doses': 2, 'eficiencia': {'1 dose':'sem dados', '2 dose': '92%'}, 'eficiencia contra variantes': '81%', 'intervalo entre as doses': '21 dias', 'efeitos colaterais':'Dor no local injeção, febre, vermelhidão, náusea e cansaço'}
+'Astrazeneca': {'numero de doses': 2, 'eficiencia por faixa etaria': 'Acima de 80 anos = 91% entre 60 e 79 anos = 93% entr 18 e 59 anos = 60% ', 'eficiencia': {'1 dose':'76%', '2 dose': '81%'}, 'eficiencia contra variantes': '92%', 'intervalo entre as doses': '3 meses', 'efeitos colaterais': 'Dor no local da injeção, dor de cabeça, fadiga, dor no corpo e mal esta'},
+'Pfizer': {'numero de doses': 2, 'eficiencia por faixa etaria': 'Acima de 55 anos = 94% Entre 16 e 55 anos = 95%', 'eficiencia': {'1 dose':'61%', '2 dose': '88%'}, 'eficiencia contra variantes': '88%', 'intervalo entre as doses': '3 meses', 'efeitos colaterais':'Dor no local da injeção, dor de cabeça, fadiga, dor no corpo e mal estar'},
+'Coronavac': {'numero de doses': 2, 'eficiencia por faixa etaria': 'Faltam dados conclusivos sobre a eficácia da Coronavac por faixa etária', 'eficiencia': {'1 dose':'sem dados%', '2 dose': '63%'}, 'eficiencia contra variantes': 'entre 69% e 78%', 'intervalo entre as doses': '21 dias', 'efeitos colaterais':'Dor no local da injeção, febre, cansaço e calafrios'},
+'Janssen': {'numero de doses': 1, 'eficiencia por faixa etaria': 'Acima de 59 anos = 76% Entre 18 e 59 anos = 64%', 'eficiencia': {'1 dose':'85%', '2 dose': 'dose única'}, 'eficiencia contra variantes': '85%', 'intervalo entre as doses': 'dose única', 'efeitos colaterais':'Dor no local da injeção, vermelhidão, dores musculares, febre, náusea, dor no peito e falta de ar'},
+'Moderna': {'numero de doses': 2, 'eficiencia por faixa etaria': 'Acima de 65 anos = 86% entre 18 e 65 anos = 95%', 'eficiencia': {'1 dose':'94%', '2 dose': '94%'}, 'eficiencia contra variantes': '94%', 'intervalo entre as doses': '28 dias', 'efeitos colaterais':'Dor no local injeção, febre, vermelhidão, náusea e cansaço'},
+'Sputnik': {'numero de doses': 2, 'eficiencia por faixa etaria': 'Os desenvolvedores da Sputnik afirmam que o imunizante possui a mesma eficacia para todas as faixas etárias', 'eficiencia': {'1 dose':'sem dados', '2 dose': '92%'}, 'eficiencia contra variantes': '81%', 'intervalo entre as doses': '21 dias', 'efeitos colaterais':'Dor no local injeção, febre, vermelhidão, náusea e cansaço'}
 }
 
 class actionEficienciaUma(Action):
@@ -41,7 +41,7 @@ class actionEficienciaUmaVariantes(Action):
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         nome = tracker.get_slot('nomevacina')
         if nome in vacinas:
-            dispatcher.utter_message(text ='A eficácia contra as variantes da ' + nome + ' é de ' + vacinas[nome][eficiencia]['1 dose'] )
+            dispatcher.utter_message(text ='A eficácia contra as variantes da ' + nome + ' é de ' + vacinas[nome]['eficiencia contra variantes'] )
         else:
             dispatcher.utter_message(text ='Vacina não encontrada na base de dados')
 
@@ -156,24 +156,40 @@ class actioneficacia1(Action):
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         nome = tracker.get_slot('nomevacina')
         if nome in vacinas:
-            dispatcher.utter_message(text ='A eficiencia da primeira dose da ' + nome + ' é: ' + vacinas[nome]['eficiencia']['1 dose'] )
+            dispatcher.utter_message(text ='A eficacia da primeira dose da ' + nome + ' é: ' + vacinas[nome]['eficiencia']['1 dose'] )
         else:
             dispatcher.utter_message(text ='Vacina não encontrada na base de dados')
+            
 
-
-class actioneficacia2(Action):
+class actionfaixaetariaall(Action): # pode ser qualquer coisa, mas por padrão utilizaremos o nome da ação
 
     def name(self) -> Text:
-        return "action_eficacia_segunda_dose"
+        return "action_faixa_etaria_todas" # precisa ser o mesmo nome da ação utilizada nas stories e domain file
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        caracteristica = 'eficiencia por faixa etaria'
+        for nome in vacinas.keys():
+            dispatcher.utter_message(text ='A vacina ' + nome + ' possui a seguinte '  + caracteristica + " : "  + str(vacinas[nome][caracteristica]))
+        return []
+
+
+class actionfaixaetariasingle(Action):
+
+    def name(self) -> Text:
+        return "action_faixa_etaria_individual"
 
     def run(self, dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         nome = tracker.get_slot('nomevacina')
         if nome in vacinas:
-            dispatcher.utter_message(text ='A eficiencia da segunda dose da ' + nome + ' é: ' + vacinas[nome]['eficiencia']['2 dose'] )
+            dispatcher.utter_message(text ='A eficacia por faixa etaria da ' + nome + ' é: ' + vacinas[nome]['eficiencia por faixa etaria'] )
         else:
             dispatcher.utter_message(text ='Vacina não encontrada na base de dados')
+
+
 
 
 
